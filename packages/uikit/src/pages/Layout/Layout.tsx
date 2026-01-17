d380dd05f620da224fa6a0358282bde35e9c4564
vladvg8 <vlad.vg8@gmail.com>
2026-01-17 18:23:21 +0300

    Remove trash

--- packages/uikit/src/pages/Layout/Layout.tsx
+++ packages/uikit/src/pages/Layout/Layout.tsx
@@ -1,13 +1,6 @@
 import { memo } from 'react';
 import './Layout.module.css';
-import { Button } from '../../components/Button/Button.tsx';
 
-const Layout = memo(() => (
-  <div>
-    <Button onClick={() => {}} disabled>
-      Привет
-    </Button>
-  </div>
-));
+const Layout = memo(() => <div></div>);
 
 export { Layout };
